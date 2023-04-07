eba4c6598138607a0ea54c8fad6bc3f4f73a7c41
Dashbloxx <dashbloxx@gmail.com>
2023-04-06 21:08:59 -0400

    Kernel: Add and finish working '/dev/random' char device. Has been tested fully

--- kernel/random.c
+++ kernel/random.c
@@ -1,3 +1,38 @@
+/*
+ *    __  __                      
+ *   |  \/  |__ _ __ _ _ __  __ _ 
+ *   | |\/| / _` / _` | '  \/ _` |
+ *   |_|  |_\__,_\__, |_|_|_\__,_|
+ *               |___/        
+ * 
+ *  Magma is a UNIX-like operating system that consists of a kernel written in C and
+ *  i?86 assembly, and userland binaries written in C.
+ *     
+ *  Copyright (c) 2023 Nexuss, John Paul Wohlscheid, rilysh, Milton612, and FueledByCocaine
+ * 
+ *  This file may or may not contain code from https://github.com/mosmeh/yagura, and/or
+ *  https://github.com/mit-pdos/xv6-public. Both projects have the same license as this
+ *  project, and the license can be seen below:
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
 #include "random.h"
 #include "api/err.h"
 #include "api/sys/sysmacros.h"
@@ -13,8 +48,13 @@
 
 static int rand_state = 1;
 
-int rand(void) {
+int rand() {
     rand_state = ((rand_state * 1103515245U) + 12345U) & 0x7fffffff;
+    return rand_state;
+}
+
+unsigned int urand() {
+    rand_state = ((rand_state * 1103515245U) + 12345U) & 0xffffffffU;
     return rand_state;
 }
 
@@ -28,8 +68,9 @@ static int write_to_bit_bucket(file_description* desc, const void* buffer, unsig
 
 static int read_random(file_description* desc, void* buffer, unsigned int count) {
     (void)desc;
-    for (unsigned int i = 0; i < count; i++) {
-        ((char*)buffer)[i] = (char)rand();
+    unsigned int* uint_buffer = (unsigned int*)buffer;
+    for (unsigned int i = 0; i < count / sizeof(unsigned int); i++) {
+        uint_buffer[i] = urand();
     }
     return count;
 }
